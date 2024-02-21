0b07881890ede3cf2e8181b0ab6cf7e9e143e9b6
ChandanaSreeSunkara <sunkarac@uwindsor.ca>
2024-02-22 02:56:54 +0530

    Added main and forgot password views

--- iadsapp/urls.py
+++ iadsapp/urls.py
@@ -1,8 +1,10 @@
-# myproject/urls.py
+# accounts/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('signin/', views.signin_view, name='signin'),
     path('signup/', views.signup_view, name='signup'),
+    path('forgot-password/<str:username>/', views.forgot_password_view, name='forgot-password'),
+    path('', views.homepage_view, name='homepage'),
 ]
