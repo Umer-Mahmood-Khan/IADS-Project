46fd366f873832b349966a6e9787f99ea8b78b6f
Chandana-Naidu <chandanasreesunkara@gmail.com>
2024-03-20 13:35:07 -0400

    Changed creds of forgot password account

--- iads/settings.py
+++ iads/settings.py
@@ -132,10 +132,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 #EMAIL
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_POST = 587
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER ='iadsprojectgame@gmail.com'
-EMAIL_HOST_PASSWORD ='iadsproject123'
+EMAIL_HOST_PASSWORD ='alar fuok aenp fmtc'
